8a12844781b3d5e326034cbd2b32f128341c1186
ayanshuDS <73418468+ayanshuDS@users.noreply.github.com>
2024-02-27 01:12:59 +0530

    moved signup functionality to the controller.js

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,5 +1,19 @@
+
+const User = require("../modal/modal");
+
 const home = (req, res) => {
     res.send("Hello World!");
 }
 
-module.exports = {home};
+const signup = async (req, res) => {
+    try {
+        await User.create(req.body);
+        res.status(200).json({
+            message: `User ${ req.body.username } created`
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+module.exports = { home, signup };
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,24 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require("../modal/modal");
 
-const {home} = require("../controllers/controller");
+const {home, signup} = require("../controllers/controller");
 
 router.get("/api", home);
 
-router.post("/api/signup", async(req,res)=>{
-
-    // const {username, password} = req.body;
-
-    // console.log(req.body);
-
-    //check if a user with this username already exists
-    await User.create(req.body);
-
-    res.json({
-        message: 'New user created successfully'
-    })
-});
+router.post("/api/signup", signup);
 
 module.exports = router;
